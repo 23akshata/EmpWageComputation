36f3835c57af1092bd26bbd2dfcee676da0783a5
akshata23 <akshata2018p@gmail.com>
2023-01-09 16:19:44 +0530

    [akshata] Find Attendance

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage");
+
+Random random = new Random();
+int result = random.Next(0, 2);
+if (result == 1)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is absent");
+}
