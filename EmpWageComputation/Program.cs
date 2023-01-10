8b097f54cb1cfa454794069d5f9d4008989af34f
akshata23 <akshata2018p@gmail.com>
2023-01-10 14:58:04 +0530

    [akshata] Calculating Emp Wage using Switch Case

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -1,22 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage");
 
+internal class UC4_EmpWageUsingSwitch
+{
+    public const int IS_PART_TIME = 1;
+    public const int IS_FULL_TIME = 2;
+    public const int EMP_RATE_PER_HOUR = 20;
+    public static void EmpWageUsingSwitch()
+    {
+        //Variables
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        //Computation
+        int empCheck = random.Next(0, 3);
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+                break;
 
-int IS_FULL_TIME = 1;
-int EMP_RATE_PER_HOUR = 20;
-//Variables
-int empHrs = 0;
-int empWage = 0;
-Random random = new Random();
-//Computation
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
-{
-    empHrs = 8;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : " + empWage);
+    }
 }
-else
-{
-    empHrs = 0;
-}
-empWage = empHrs * EMP_RATE_PER_HOUR;
-Console.WriteLine("Emp Wage : " + empWage);
+
