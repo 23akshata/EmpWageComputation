927040feae6f31c05c99eaea14e733b6fc7f378a
akshata23 <akshata2018p@gmail.com>
2023-01-10 14:08:14 +0530

    [akshata] calculating part time wage

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -2,21 +2,21 @@
 Console.WriteLine("Welcome to Employee Wage");
 
 
-    int IS_FULL_TIME = 1;
-    int EMP_RATE_PER_HOUR = 20;
-    //Variables
-    int empHrs = 0;
-    int empWage = 0;
-    Random random = new Random();
-    //Computation
-    int empCheck = random.Next(0, 2);
-    if (empCheck == IS_FULL_TIME)
-    {
-        empHrs = 8;
-    }
-    else
-    {
-        empHrs = 0;
-    }
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    Console.WriteLine("Emp Wage : " + empWage);
+int IS_FULL_TIME = 1;
+int EMP_RATE_PER_HOUR = 20;
+//Variables
+int empHrs = 0;
+int empWage = 0;
+Random random = new Random();
+//Computation
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
+{
+    empHrs = 8;
+}
+else
+{
+    empHrs = 0;
+}
+empWage = empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage : " + empWage);
